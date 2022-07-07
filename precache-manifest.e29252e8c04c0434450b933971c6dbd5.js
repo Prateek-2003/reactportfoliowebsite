9176817897b98138535ba6bea0546e1d7b292941
Prateek Singh <102.prateeksingh@gmail.com>
2022-07-07 18:15:44 +0530

    Updates

---
+++ precache-manifest.e29252e8c04c0434450b933971c6dbd5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4ea8fdfb7bd7dd0798",
+    "url": "/reactportfoliowebsite/static/css/main.05635d1c.chunk.css"
+  },
+  {
+    "revision": "9d4ea8fdfb7bd7dd0798",
+    "url": "/reactportfoliowebsite/static/js/main.9d4ea8fd.chunk.js"
+  },
+  {
+    "revision": "b0603662954d921478cb",
+    "url": "/reactportfoliowebsite/static/js/1.b0603662.chunk.js"
+  },
+  {
+    "revision": "a7b0d212e1bca8552670",
+    "url": "/reactportfoliowebsite/static/js/runtime~main.a7b0d212.js"
+  },
+  {
+    "revision": "c86b55e8570cf0c9fe246961312170a8",
+    "url": "/reactportfoliowebsite/static/media/man2.c86b55e8.png"
+  },
+  {
+    "revision": "7064f4aa70837d7fcf5e1f6051028248",
+    "url": "/reactportfoliowebsite/static/media/avatar1.7064f4aa.png"
+  },
+  {
+    "revision": "3bd2bc26702262af781bb63284d84b8c",
+    "url": "/reactportfoliowebsite/static/media/demoresume.3bd2bc26.pdf"
+  },
+  {
+    "revision": "228175fc044b295809a71417b1dc289e",
+    "url": "/reactportfoliowebsite/static/media/img1.228175fc.webp"
+  },
+  {
+    "revision": "52b3b87f5ebf8e4d77c0efb0ae1ab17d",
+    "url": "/reactportfoliowebsite/static/media/img2.52b3b87f.webp"
+  },
+  {
+    "revision": "c262f8f5e989d2968d168e5fa873379d",
+    "url": "/reactportfoliowebsite/static/media/img3.c262f8f5.webp"
+  },
+  {
+    "revision": "32382638a90b9e47564cd0265d61ff59",
+    "url": "/reactportfoliowebsite/index.html"
+  }
+];
