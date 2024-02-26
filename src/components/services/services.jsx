63907a215a315ac9cadf5af833255e706f138d81
Prateek Singh <104994924+Prateek-2003@users.noreply.github.com>
2024-02-26 17:10:29 +0530

    Update services.jsx

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -5,78 +5,78 @@ import {BiCheck} from 'react-icons/bi'
 const services = () => {
   return (
     <section id='services'>
-      <h5>What I offer</h5>
-      <h2>Services</h2>
+      <h5>My Background</h5>
+      <h2>Education</h2>
 
       <div className="conainer services_container">
         <article className='service'>
           <div className="services_head">
-            <h3>UI/Ux Design</h3>
+            <h3>B.Tech. 2020-2024</h3>
           </div>
           <ul className='service_list'>
-            <li>
+          <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Currently Pursuing </p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>From - Shri Ramswaroop Memorial University</p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Stream - Computer Science</p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>CGPA - 7.5 </p>
             </li>
           </ul>
         </article>
 
         <article className='service'>
           <div className="services_head">
-            <h3>Web Deveplopment</h3>
+            <h3>Intermediate 2019-2020</h3>
           </div>
           <ul className='service_list'>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Passed out from CBSE</p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>From - Central Academy</p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Stream - Physics,Chemistry,Maths</p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Percentage - 74.2%</p>
             </li>
           </ul>
         </article>
 
         <article className='service'>
           <div className="services_head">
-            <h3>Content Creation</h3>
+            <h3>High School 2017-2018</h3>
           </div>
           <ul className='service_list'>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Passed out from CBSE</p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>From - Central Academy</p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Stream - Science</p>
             </li>
             <li>
               <BiCheck className='service_list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur elit.</p>
+              <p>Percentage - 79%</p>
             </li>
           </ul>
         </article>
